e5779ce1b77f9d59b16af931b74fb9d54fce130c
Dimitrije <zivanovic2601@gmail.com>
2022-01-03 02:13:09 +0100

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'aa7da7f9-de20-40ca-9a57-0c87eabf11cd';
+export const MODAL_CLOSE_SEC = 2.5;
